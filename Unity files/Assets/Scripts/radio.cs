725a9c936ca5bef0803eb41699cdb563e3cbf2ba
Martin Beyrle <26486705+made3@users.noreply.github.com>
2017-08-18 18:04:01 +0200

    Started HUD
    "Crosshair" added in canvas

--- Unity files/Assets/Scripts/radio.cs
+++ Unity files/Assets/Scripts/radio.cs
@@ -7,43 +7,125 @@ public class radio : MonoBehaviour {
 
     public string myString;
     public Text myText1;
-    public float fadeTime;
+    public float textFadingSpeed;
     public bool displayInfo;
-    private AudioSource _audioSource;
-    public bool isOn;
+    public float songFadingSpeed;
+    public float rauschenFadingSpeed;
+    private AudioSource _rauschen;
+    private float tmpRauschTime;
+    private AudioSource _song1;
+    private bool isOn;
+    private bool isFading;
+    private bool startSong;
 
 
     // Use this for initialization
     void Start () {
         myText1.color = Color.clear;
-        _audioSource = GetComponent<AudioSource>();
+
+        var audioSources = GetComponents<AudioSource>();
+        _rauschen = audioSources[1];
+        _rauschen.volume = 0;
+        _song1 = audioSources[0];
+        _song1.volume = 0;
+        tmpRauschTime = 0;
+
         isOn = false;
+        isFading = false;
+        startSong = false;
     }
 
     // Update is called once per frame
     void Update () {
 
         FadeText();
+        activateRadio();
 
-        if (displayInfo)
+    }
+
+    void activateRadio()
+    {
+        if (displayInfo && !isFading)
         {
 
             if (Input.GetKeyDown(KeyCode.E))
             {
                 if (isOn)
                 {
-                    _audioSource.Pause();
                     isOn = false;
+                    isFading = true;
                 }
                 else
                 {
-                    _audioSource.Play();
                     isOn = true;
+                    isFading = true;
                 }
 
             }
         }
 
+        if (isOn && isFading)
+        {
+            playNoise();
+
+            if (startSong)
+            {
+                if (_song1.volume < 1)
+                {
+                    _song1.volume = _song1.volume + Time.deltaTime * songFadingSpeed;
+                }
+                else
+                {
+                    tmpRauschTime = 0;
+                    startSong = false;
+                    isFading = false;
+                }
+            }
+        }
+
+        if (!isOn && isFading)
+        {
+            
+            playNoise();
+            
+            if(startSong)
+            {
+                if (_song1.volume > 0)
+                {
+                    _song1.volume = _song1.volume - Time.deltaTime * songFadingSpeed;
+                }
+                else
+                {
+                    tmpRauschTime = 0;
+                    startSong = false;
+                    isFading = false;
+                }
+            }
+        }
+    }
+
+    void playNoise()
+    {
+        if (tmpRauschTime < 1)
+        {
+            tmpRauschTime += Time.deltaTime;
+            _rauschen.volume = _rauschen.volume + Time.deltaTime * rauschenFadingSpeed;
+            if (_rauschen.volume >= 0.2 && !isOn)
+            {
+                Debug.Log("NUMMER 1");
+                startSong = true;
+            }
+        }
+        else if(tmpRauschTime >= 1)
+        {
+            _rauschen.volume = _rauschen.volume - Time.deltaTime * rauschenFadingSpeed;
+            if(_rauschen.volume <= 0.5 && isOn)
+            {
+                Debug.Log("NUMMER 2");
+                startSong = true;
+            }
+        }
+        
     }
 
     void OnMouseOver()
@@ -58,22 +140,22 @@ public class radio : MonoBehaviour {
 
     void FadeText()
     {
-        if (displayInfo)
+        if (displayInfo && !isFading)
         {
             if (isOn)
             {
                 myText1.text = "Press E to turn off";
-                myText1.color = Color.Lerp(myText1.color, Color.white, fadeTime * Time.deltaTime);
+                myText1.color = Color.Lerp(myText1.color, Color.white, textFadingSpeed * Time.deltaTime);
             }
             else
             {
                 myText1.text = myString;
-                myText1.color = Color.Lerp(myText1.color, Color.white, fadeTime * Time.deltaTime);
+                myText1.color = Color.Lerp(myText1.color, Color.white, textFadingSpeed * Time.deltaTime);
             }
         }
         else
         {
-            myText1.color = Color.Lerp(myText1.color, Color.clear, fadeTime * Time.deltaTime);
+            myText1.color = Color.Lerp(myText1.color, Color.clear, textFadingSpeed * Time.deltaTime);
         }
     }
 }
