9931e04600c47d3de6c3e3fccc722206c3b86658
Martin Beyrle <26486705+made3@users.noreply.github.com>
2018-09-03 16:42:30 +0200

    Office for start menu created, start menu implemented in Unity

---
+++ Unity files/Assets/Scripts/Startmenu/Startmenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Startmenu : MonoBehaviour {
+
+    private int currentPage;
+
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        currentPage = anim.GetInteger("currentPage");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (currentPage > 0) currentPage--;
+            anim.SetInteger("currentPage", currentPage);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (currentPage < 3) currentPage++;
+            anim.SetInteger("currentPage", currentPage);
+        }
+    }
+}
---
+++ Unity files/Assets/Scripts/Startmenu/Startmenu_Camera.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Startmenu_Camera : MonoBehaviour
+{
+
+    Vector2 mouseLook;
+    Vector2 mouseLookRaw;
+    Vector2 smoothV;
+    public float sensitivity = 5.0f;
+    public float smoothing = 5.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        mouseLookRaw += md;
+        md.x *= sensitivity;
+        md.y *= sensitivity;
+        smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f / smoothing);
+        smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f / smoothing);
+        mouseLook += smoothV;
+
+        mouseLook.y = Mathf.Clamp(mouseLook.y, -90f, 90f);
+
+        if(mouseLook.x > 40) mouseLook.x = 40;
+        else if(mouseLook.x < -40) mouseLook.x = -40;
+
+        if (mouseLook.y > 6) mouseLook.y = 6;
+        else if (mouseLook.y < -45) mouseLook.y = -45;
+
+        transform.localRotation = Quaternion.Euler(-mouseLook.y, mouseLook.x, transform.localRotation.eulerAngles.z);
+    }
+}
